40de9b759840ac034cfd2057fceab498af1adace
Michael Schwartz <mykesx@gmail.com>
2014-10-07 17:47:16 -0700

    debug

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var JSXTransformer = require('react/JSXTransformer');
 include.paths.push('./jsx');
 require.paths.push('./jsx');
 require.extensions.jsx = include.extensions.jsx = function(source, filename) {
-    return JSXTransformer.transform(source);
+    return JSXTransformer.transform(source).code;
 };
