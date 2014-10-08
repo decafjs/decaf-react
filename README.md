4cc3b5e8ff311f828ba8655c99a3f51cd11c1fca
Michael Schwartz <mykesx@gmail.com>
2014-10-07 17:36:34 -0700

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ decaf-react
 ===========
 
 React JS language module for DecafJS
+
+This sets up DecafJS require() and include() to automatically compile/translate React JSX source files upon load.
+
---
+++ index.js
@@ -0,0 +1,8 @@
+global.React = require('react/react');
+var JSXTransformer = require('react/JSXTransformer');
+
+include.paths.push('./jsx');
+require.paths.push('./jsx');
+require.extensions.jsx = include.extensions.jsx = function(source, filename) {
+    return JSXTransformer.transform(source);
+};
