536caeed2e3903549a54abdc5b7bc4748d70339e
Michael Schwartz <mykes@sportstwo.com>
2014-10-10 17:34:37 -0700

    working example

--- examples/server.js
+++ examples/server.js
@@ -1,5 +1,5 @@
-var http = require('http'),
-    File = require('File'),
+var http  = require('http'),
+    File  = require('File'),
     React = require('react'),
     // This is our React component, shared by server and browser thanks to browserify
     MyApp = require('./myApp');
@@ -7,90 +7,97 @@ var http = require('http'),
 // Just create a plain old HTTP server that responds to two endpoints ('/' and
 // '/bundle.js') This would obviously work similarly with any higher level
 // library (Express, etc)
-http.createServer(function(req, res) {
-  // If we hit the homepage, then we want to serve up some HTML - including the
-  // server-side rendered React component(s), as well as the script tags
-  // pointing to the client-side code
-  if (req.uri == '/') {
+http.createServer(function (req, res) {
+    // If we hit the homepage, then we want to serve up some HTML - including the
+    // server-side rendered React component(s), as well as the script tags
+    // pointing to the client-side code
+    if (req.uri == '/') {
 
-    // This represents our data to be passed in to the React component for
-    // rendering - just as you would pass data, or expose variables in
-    // templates such as Jade or Handlebars.  We just use an array of garbage
-    // here (with some potentially dangerous values for testing), but you could
-    // imagine this would be objects typically fetched async from a DB,
-    // filesystem or API, depending on the logged-in user, etc.
-    var props = {items: [0, 1, '</script>', '<!--inject!-->']}
+        // This represents our data to be passed in to the React component for
+        // rendering - just as you would pass data, or expose variables in
+        // templates such as Jade or Handlebars.  We just use an array of garbage
+        // here (with some potentially dangerous values for testing), but you could
+        // imagine this would be objects typically fetched async from a DB,
+        // filesystem or API, depending on the logged-in user, etc.
+        var props = {items: [0, 1, '</script>', '<!--inject!-->']}
 
-    // Now that we've got our data, we can perform the server-side rendering by
-    // passing it in as `props` to our React component - and returning an HTML
-    // string to be sent to the browser
-    var myAppHtml = React.renderComponentToString(MyApp(props))
+        // Now that we've got our data, we can perform the server-side rendering by
+        // passing it in as `props` to our React component - and returning an HTML
+        // string to be sent to the browser
+        var myAppHtml = React.renderComponentToString(MyApp(props))
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {'Content-Type': 'text/html'});
 
-    // Now send our page content - this could obviously be constructed in
-    // another template engine, or even as a top-level React component itself -
-    // but easier here just to construct on the fly
-    res.end(
-      // <html>, <head> and <body> are for wusses
+        // Now send our page content - this could obviously be constructed in
+        // another template engine, or even as a top-level React component itself -
+        // but easier here just to construct on the fly
+        res.end(
+            // <html>, <head> and <body> are for wusses
 
-      // Include our static React-rendered HTML in our content div. This is
-      // the same div we render the component to on the client side, and by
-      // using the same initial data, we can ensure that the contents are the
-      // same (React is smart enough to ensure no rendering will actually occur
-      // on page load)
-      '<div id=content>' + myAppHtml + '</div>' +
+            // Include our static React-rendered HTML in our content div. This is
+            // the same div we render the component to on the client side, and by
+            // using the same initial data, we can ensure that the contents are the
+            // same (React is smart enough to ensure no rendering will actually occur
+            // on page load)
+            '<div id=content>' + myAppHtml + '</div>' +
 
-      // We'll load React from a CDN - you don't have to do this,
-      // you can bundle it up or serve it locally if you like
-      '<script src=//fb.me/react-0.11.2.min.js></script>' +
+                // We'll load React from a CDN - you don't have to do this,
+                // you can bundle it up or serve it locally if you like
+            '<script src=//fb.me/react-0.11.2.min.js></script>' +
 
-      // Then the browser will fetch the browserified bundle, which we serve
-      // from the endpoint further down. This exposes our component so it can be
-      // referenced from the next script block
-      '<script src=/bundle.js></script>' +
+                // Then the browser will fetch the browserified bundle, which we serve
+                // from the endpoint further down. This exposes our component so it can be
+                // referenced from the next script block
+            '<script src=/bundle.js></script>' +
 
-      // This script renders the component in the browser, referencing it
-      // from the browserified bundle, using the same props we used to render
-      // server-side. We could have used a window-level variable, or even a
-      // JSON-typed script tag, but this option is safe from namespacing and
-      // injection issues, and doesn't require parsing
-      '<script>' +
-        'var MyApp = require("./myApp.js"), container = document.getElementById("content"); ' +
-        'React.renderComponent(MyApp(' + safeStringify(props) + '), container)' +
-      '</script>'
-    )
+                // This script renders the component in the browser, referencing it
+                // from the browserified bundle, using the same props we used to render
+                // server-side. We could have used a window-level variable, or even a
+                // JSON-typed script tag, but this option is safe from namespacing and
+                // injection issues, and doesn't require parsing
+            '<script>' +
+            'var MyApp = require("./myApp.js"), container = document.getElementById("content"); ' +
+            'React.renderComponent(MyApp(' + safeStringify(props) + '), container)' +
+            '</script>'
+        )
 
-  // This endpoint is hit when the browser is requesting bundle.js from the page above
-  } else if (req.uri == '/bundle.js') {
-    var bundle = new File('bower_components/decaf-jolt-require/require.js').readAll();
-    var    content = require.getContent('./myApp').content;
-    bundle += "require.register('react', 'module.exports = window.React;');";
-    bundle += "require.register('./myApp.js', ";
-    bundle += JSON.stringify(content)
-    bundle += ');'
+        // This endpoint is hit when the browser is requesting bundle.js from the page above
+    } else if (req.uri == '/bundle.js') {
+        var Bundler = require('decaf-jolt-require').Bundler;
 
-    res.writeHead(200, { 'Content-Type' : 'text/javascript'})
+        //var bundle = new File('bower_components/decaf-jolt-require/require.js').readAll();
+        //var content = require.getContent('./myApp').content;
+        //bundle += '__require__();\n';
+        //bundle += "require.register('react', 'module.exports = window.React;');\n";
+        //bundle += "require.register('./myApp.js', ";
+        //bundle += JSON.stringify(content)
+        //bundle += ');\n';
 
-    // Here we invoke browserify to package up our component.
-    // DON'T do it on the fly like this in production - it's very costly -
-    // either compile the bundle ahead of time, or use some smarter middleware
-    // (eg browserify-middleware).
-    // We also use literalify to transform our `require` statements for React
-    // so that it uses the global variable (from the CDN JS file) instead of
-    // bundling it up with everything else
-    res.end(bundle);
-    // browserify()
-    //   .transform(literalify.configure({react: 'window.React'}))
-    //   .require('./myApp.js')
-    //   .bundle()
-    //   .pipe(res)
+        res.writeHead(200, {'Content-Type': 'text/javascript'})
 
-  // Return 404 for all other requests
-  } else {
-    res.statusCode = 404
-    res.end()
-  }
+        // Here we invoke browserify to package up our component.
+        // DON'T do it on the fly like this in production - it's very costly -
+        // either compile the bundle ahead of time, or use some smarter middleware
+        // (eg browserify-middleware).
+        // We also use literalify to transform our `require` statements for React
+        // so that it uses the global variable (from the CDN JS file) instead of
+        // bundling it up with everything else
+        debugger;
+        var bundler = new Bundler()
+            .literal('react', 'window.React')
+            .require('./myApp.js');
+        res.end(bundler.bundle());
+        // browserify()
+        //   .transform(literalify.configure({react: 'window.React'}))
+        //   .require('./myApp.js')
+        //   .bundle()
+        //   .pipe(res)
+
+        // Return 404 for all other requests
+    } else {
+        res.statusCode = 404
+        res.end()
+    }
 
 // The http server listens on port 3000
 }).listen(3000, '127.0.0.1')
@@ -98,5 +105,5 @@ http.createServer(function(req, res) {
 
 // A utility function to safely escape JSON for embedding in a <script> tag
 function safeStringify(obj) {
-  return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
+    return JSON.stringify(obj).replace(/<\/script/g, '<\\/script').replace(/<!--/g, '<\\!--')
 }
